3421c10c6448476327a24e00e87a4e51934df515
John Wason <wason@wasontech.com>
2021-03-05 11:19:55 -0500

    Upgrade to emscripten2x

--- build_robotraconteur_pyodide.sh
+++ build_robotraconteur_pyodide.sh
@@ -5,7 +5,7 @@ set -e
 PYODIDE_ROOT=/src
 PYODIDE_PACKAGE_ABI=1
 
-BOOST_VERSION=1.71.0
+BOOST_VERSION=1.75.0
 
 CURRENT_DIR=$(dirname "$(readlink -f '$0')")
 
@@ -21,8 +21,8 @@ BOOST_LIBS_COMMA=$(echo $BOOST_LIBS | sed -e 's/[[:space:]]/,/g')
 
 source $PYODIDE_ROOT/emsdk/emsdk/emsdk_env.sh
 
-SIDE_C_FLAGS="-s DISABLE_EXCEPTION_CATCHING=0 -s EXCEPTION_DEBUG=0 -s ASSERTIONS=0 -fpic -O2 -std=c++14 -s SIDE_MODULE=1"
-SIDE_LDFLAGS="-s \"BINARYEN_METHOD='native-wasm'\" -Werror -s EMULATED_FUNCTION_POINTERS=1 -s EMULATE_FUNCTION_POINTER_CASTS=1 -s WASM=1  --memory-init-file 0 -s EXPORT_ALL=1 -s DISABLE_EXCEPTION_CATCHING=0 -s EXCEPTION_DEBUG=0 -s ASSERTIONS=0 -O2 -std=c++14 -s SIDE_MODULE=1 -lwebsocket.js"
+SIDE_C_FLAGS="-s DISABLE_EXCEPTION_CATCHING=0 -s EXCEPTION_DEBUG=0 -s ASSERTIONS=1 -fpic -O1 -std=c++14 -s WASM=1 -lwebsocket.js -lhtml5.js"
+SIDE_LDFLAGS="-s \"BINARYEN_METHOD='native-wasm'\" -Werror -s EMULATED_FUNCTION_POINTERS=1 -s EMULATE_FUNCTION_POINTER_CASTS=1 -s WASM=1  -s DISABLE_EXCEPTION_CATCHING=0 -s EXCEPTION_DEBUG=0 -s ASSERTIONS=1 -O1 -std=c++14"
 
 #
 #-s \"BINARYEN_TRAP_MODE='clamp'\"
@@ -38,7 +38,7 @@ if [ ! -s $BOOST_BUILD_DIR/b2 ]; then
 fi
 
 if [ ! -s $BOOST_BUILD_DIR/stage/lib/libboost_regex.bc ]; then
-	( cd $BOOST_BUILD_DIR && $BOOST_BUILD_DIR/b2 toolset=emscripten link=static --compileflags="$SIDE_C_FLAGS" --linkflags="-fpic $SIDE_LDFLAGS" --with-date_time --with-filesystem --with-system --with-regex --with-chrono --with-random --with-program_options --disable-icu )
+	( cd $BOOST_BUILD_DIR && $BOOST_BUILD_DIR/b2 toolset=emscripten link=static threading=single cxxflags="$SIDE_C_FLAGS" cflags="$SIDE_C_FLAGS" linkflags="-fpic $SIDE_LDFLAGS" --with-date_time --with-filesystem --with-system --with-regex --with-chrono --with-random --with-program_options --disable-icu -a )
 fi
 
 mkdir -p build
@@ -50,7 +50,7 @@ if [ -f $CURRENT_DIR/build/CMakeCache.txt ]; then
 fi
 
 (cd $CURRENT_DIR/build && cmake .. -DCMAKE_TOOLCHAIN_FILE=$PYODIDE_ROOT/emsdk/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DBUILD_PYTHON=ON -DBOOST_INCLUDEDIR=$BOOST_BUILD_DIR \
--DBOOST_LIBRARYDIR=$BOOST_BUILD_DIR/stage/lib -DBoost_ADDITIONAL_VERSIONS="1.71;1.71.0" \
+-DBOOST_LIBRARYDIR=$BOOST_BUILD_DIR/stage/lib -DBoost_ADDITIONAL_VERSIONS="1.75;1.75.0" \
 -DBoost_DATE_TIME_LIBRARY_RELEASE=$BOOST_LIB_DIR/libboost_date_time.bc -DBoost_DATE_TIME_LIBRARY_DEBUG=$BOOST_LIB_DIR/libboost_date_time.bc \
 -DBoost_FILESYSTEM_LIBRARY_RELEASE=$BOOST_LIB_DIR/libboost_filesystem.bc -DBoost_FILESYSTEM_LIBRARY_DEBUG=$BOOST_LIB_DIR/libboost_filesystem.bc \
 -DBoost_SYSTEM_LIBRARY_RELEASE=$BOOST_LIB_DIR/libboost_system.bc -DBoost_SYSTEM_LIBRARY_DEBUG=$BOOST_LIB_DIR/libboost_system.bc \
@@ -71,18 +71,18 @@ fi
 
 ( cd $CURRENT_DIR/build && make VERBOSE=1 -j1 )
 
-#PYODIDE_RR_DIR=$PYODIDE_ROOT/root/lib/python3.8/site-packages/RobotRaconteur
+PYODIDE_RR_DIR=$PYODIDE_ROOT/root/lib/python3.8/site-packages/RobotRaconteur
 
-#mkdir -p $PYODIDE_RR_DIR
+mkdir -p $PYODIDE_RR_DIR
 
-#cp $CURRENT_DIR/build/out/Python/RobotRaconteur/*.py $PYODIDE_RR_DIR
+cp $CURRENT_DIR/build/out/Python/RobotRaconteur/*.py $PYODIDE_RR_DIR
 
-#touch $PYODIDE_ROOT/root/.rrbuilt
+touch $PYODIDE_ROOT/root/.rrbuilt
 
-#( cd $PYODIDE_ROOT && make )
+( cd $PYODIDE_ROOT && make )
 
-( cd $CURRENT_DIR/build/out/Python && python setup.py install --root /rr_src/build/py_install --prefix= --single-version-externally-managed )
-( cd $CURRENT_DIR/build/py_install && python /src/emsdk/emsdk/upstream/emscripten/tools/file_packager.py ../robotraconteur.data --js-output=../robotraconteur.js --lz4 --preload $CURRENT_DIR/build/py_install/@ --export-name=pyodide._module --exclude *.wasm.pre --exclude *__pycache** --use-preload-plugins )
+# ( cd $CURRENT_DIR/build/out/Python && python setup.py install --root /rr_src/build/py_install --prefix= --single-version-externally-managed )
+# ( cd $CURRENT_DIR/build/py_install && python /src/emsdk/emsdk/upstream/emscripten/tools/file_packager.py ../robotraconteur.data --js-output=../robotraconteur.js --lz4 --preload $CURRENT_DIR/build/py_install/@ --export-name=pyodide._module --exclude *.wasm.pre --exclude *__pycache** --use-preload-plugins )
 
-cp $CURRENT_DIR/build/robotraconteur.data $PYODIDE_ROOT/build/
-cp $CURRENT_DIR/build/robotraconteur.js $PYODIDE_ROOT/build/
+# cp $CURRENT_DIR/build/robotraconteur.data $PYODIDE_ROOT/build/
+# cp $CURRENT_DIR/build/robotraconteur.js $PYODIDE_ROOT/build/
